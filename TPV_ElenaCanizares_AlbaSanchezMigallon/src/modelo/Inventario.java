24858fd201676c10ad71873d8f8a36e4a813fd93
AlbaSMigallon <albasmigallon@gmail.com>
2024-01-11 10:47:26 +0100

    Creación de clases

---
+++ TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Inventario.java
@@ -0,0 +1,11 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Inventario {
+	//contiene las litas de productos
+	private ArrayList<Vino> stockVinos;
+	private ArrayList<Cerveza> stockCervezas;
+	private ArrayList<Refresco> stockRefrescos;
+	private ArrayList<Ingrediente> stockIngredientes;
+}
---
+++ TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Pedido.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class Pedido {
+	private long idMesa;
+	private Pedido pedido;
+	private boolean esOcupada;
+
+}
