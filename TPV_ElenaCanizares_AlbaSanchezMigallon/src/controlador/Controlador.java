209a910d04c17864c1b3a4d6e28a34b35c8f2d32
elena <ecanizaresj99@gmail.com>
2024-01-23 12:48:51 +0100

    Enlace clases de datos

--- TPV_ElenaCanizares_AlbaSanchezMigallon/src/controlador/Controlador.java
+++ TPV_ElenaCanizares_AlbaSanchezMigallon/src/controlador/Controlador.java
@@ -1,13 +1,36 @@
 package controlador;
 
 import vista.Vista;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import vista.Vista;
+import modelo.Barra;
+import modelo.Botella;
+import modelo.Cerveza;
+import modelo.Coctel;
+import modelo.Ingrediente;
+import modelo.Inventario;
+import modelo.Mesa;
+import modelo.Musica;
+import modelo.Pedido;
+import modelo.Refresco;
+import modelo.Vino;
 
 public class Controlador implements ActionListener {
 	Vista vista = new Vista();
+	Barra barra = new Barra();
+	Botella botella = new Botella();
+	Cerveza cerveza = new Cerveza();
+	Coctel coctel = new Coctel();
+	Ingrediente ingrediente = new Ingrediente();
+	Inventario inventario = new Inventario();
+	Mesa mesa = new Mesa();
+	Musica musica = new Musica();
+	Pedido pedido = new Pedido();
+	Refresco refresco = new Refresco();
+	Vino vino = new Vino();
 
 	public Controlador(Vista vista) {
 		this.vista = vista;
--- TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Barra.java
+++ TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Barra.java
@@ -2,6 +2,7 @@ package modelo;
 
 import java.util.ArrayList;
 
+
 public class Barra {
 	ArrayList<Pedido> pedidosBarra;
 }
