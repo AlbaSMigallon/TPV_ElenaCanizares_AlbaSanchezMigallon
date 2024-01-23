556294f4476789c566dec79769f5329c761931eb
elena <ecanizaresj99@gmail.com>
2024-01-23 20:18:32 +0100

    Empezar vista

--- TPV_ElenaCanizares_AlbaSanchezMigallon/src/vista/Vista.java
+++ TPV_ElenaCanizares_AlbaSanchezMigallon/src/vista/Vista.java
@@ -1,5 +1,6 @@
 package vista;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -7,6 +8,9 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import controlador.Controlador;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class Vista extends JFrame {
 
@@ -40,6 +44,19 @@ public class Vista extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		JPanel panelInicio = new JPanel();
+		panelInicio.setBounds(0, 0, 1684, 861);
+		contentPane.add(panelInicio);
+		panelInicio.setLayout(null);
+
+		JButton btnMesa1 = new JButton();
+		btnMesa1.setIcon(new ImageIcon("src/resources/mesaPrueba.png"));
+		btnMesa1.setBounds(147, 66, 203, 141);
+		btnMesa1.setOpaque(false);
+		btnMesa1.setContentAreaFilled(false);
+		btnMesa1.setBorderPainted(false);
+		panelInicio.add(btnMesa1);
+	}
 }
