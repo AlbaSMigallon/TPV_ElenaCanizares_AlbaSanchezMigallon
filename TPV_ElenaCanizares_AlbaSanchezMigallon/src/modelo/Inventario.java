197bdab3ce8795e5ee45ba42fbbdfccfe251d8de
AlbaSMigallon <albasmigallon@gmail.com>
2024-02-03 20:44:47 +0100

    Metodos hashcode y equals en producto para gestion inventario

--- TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Inventario.java
+++ TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Inventario.java
@@ -3,31 +3,129 @@ package modelo;
 import java.util.HashMap;
 
 public class Inventario {
-	private Refresco refresco; // Agregamos una instancia de refresco para prueba
-	private Cerveza cerveza;
-	private Aperitivo aperitivo;
-	private Botella botella;
-	private Coctel cocktel;
-	private Ingrediente ingrediente;
-	private Vino vino;
-	private HashMap<String, Integer> inventario;
+	// clase con un hashmap por tipo de producto en clave y en valor la cantidad de stock
 
-	public Inventario(Refresco refresco, Cerveza cerveza, Aperitivo aperitivo, Botella botella, Coctel cocktel,
-			Ingrediente ingrediente, Vino vino, HashMap<String, Integer> inventario) {
-		this.refresco = refresco;
-		this.cerveza = cerveza;
-		this.aperitivo = aperitivo;
-		this.botella = botella;
-		this.cocktel = cocktel;
-		this.ingrediente = ingrediente;
-		this.vino = vino;
-		this.inventario = inventario;
-		this.inventario = new HashMap<>();
-	}
-
+	private HashMap<Vino, Integer> vinos;
+	private HashMap<Cerveza, Integer> cervezas;
+	private HashMap<Aperitivo, Integer> aperitivos;
+	private HashMap<Botella, Integer> botellas;
+	private HashMap<Ingrediente, Integer> ingredientes;
+	private HashMap<Refresco, Integer> refrescos;
+	
 	public Inventario() {
-
+		super();
+		this.vinos = new HashMap<>();
+		this.cervezas = new HashMap<>();
+		this.aperitivos = new HashMap<>();
+		this.botellas = new HashMap<>();
+		this.ingredientes = new HashMap<>();
+		this.refrescos = new HashMap<>();
 	}
+
+	public HashMap<Vino, Integer> getVinos() {
+		return vinos;
+	}
+
+	public void setVinos(HashMap<Vino, Integer> vinos) {
+		this.vinos = vinos;
+	}
+
+	public HashMap<Cerveza, Integer> getCervezas() {
+		return cervezas;
+	}
+
+	public void setCervezas(HashMap<Cerveza, Integer> cervezas) {
+		this.cervezas = cervezas;
+	}
+
+	public HashMap<Aperitivo, Integer> getAperitivos() {
+		return aperitivos;
+	}
+
+	public void setAperitivos(HashMap<Aperitivo, Integer> aperitivos) {
+		this.aperitivos = aperitivos;
+	}
+
+	public HashMap<Botella, Integer> getBotellas() {
+		return botellas;
+	}
+
+	public void setBotellas(HashMap<Botella, Integer> botellas) {
+		this.botellas = botellas;
+	}
+
+	public HashMap<Ingrediente, Integer> getIngredientes() {
+		return ingredientes;
+	}
+
+	public void setIngredientes(HashMap<Ingrediente, Integer> ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	public HashMap<Refresco, Integer> getRefrescos() {
+		return refrescos;
+	}
+
+	public void setRefrescos(HashMap<Refresco, Integer> refrescos) {
+		this.refrescos = refrescos;
+	}
+	
+	
+
+    // Métodos para manipular el inventario
+
+    public void agregarVino(Vino vino, int cantidad) {
+        vinos.put(vino, cantidad);
+    }
+
+    public void agregarCerveza(Cerveza cerveza, int cantidad) {
+        cervezas.put(cerveza, cantidad);
+    }
+
+    public void agregarAperitivo(Aperitivo aperitivo, int cantidad) {
+        aperitivos.put(aperitivo, cantidad);
+    }
+
+    public void agregarBotella(Botella botella, int cantidad) {
+        botellas.put(botella, cantidad);
+    }
+
+    public void agregarIngrediente(Ingrediente ingrediente, int cantidad) {
+        ingredientes.put(ingrediente, cantidad);
+    }
+
+    public void agregarRefresco(Refresco refresco, int cantidad) {
+        refrescos.put(refresco, cantidad);
+    }
+
+    public int obtenerCantidadVino(Vino vino) {
+        return vinos.getOrDefault(vino, 0);
+    }
+
+    public int obtenerCantidadCerveza(Cerveza cerveza) {
+        return cervezas.getOrDefault(cerveza, 0);
+    }
+
+    public int obtenerCantidadAperitivo(Aperitivo aperitivo) {
+        return aperitivos.getOrDefault(aperitivo, 0);
+    }
+
+    public int obtenerCantidadBotella(Botella botella) {
+        return botellas.getOrDefault(botella, 0);
+    }
+
+    public int obtenerCantidadIngrediente(Ingrediente ingrediente) {
+        return ingredientes.getOrDefault(ingrediente, 0);
+    }
+
+    public int obtenerCantidadRefresco(Refresco refresco) {
+        return refrescos.getOrDefault(refresco, 0);
+    }
+	
+	
+
+
+/*
 
 	public void agregarProducto(String nombreBebida, int cantidad) {
 		inventario.put(nombreBebida, cantidad);
@@ -48,4 +146,6 @@ public class Inventario {
 		}
 
 	}
+	
+	*/
 }
--- TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Producto.java
+++ TPV_ElenaCanizares_AlbaSanchezMigallon/src/modelo/Producto.java
@@ -1,6 +1,46 @@
 package modelo;
 
+import java.util.Objects;
+
 public abstract class Producto {
+	protected double precio;
+	protected String nombre;
+	
+	public Producto(double precio, String nombre) {
+		// TODO Auto-generated constructor stub
+		this.nombre= nombre;
+		this.precio= precio;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	//para manejar que los objetos que extienden de producto puedan estar dentro de un hashmap
+	@Override
+	public int hashCode() {
+		return Objects.hash(nombre, precio);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		return Objects.equals(nombre, other.nombre) && precio == other.precio;
+	}
+	
 	
 
 }
